61eb6f8ed79edb9d9f8e5e2333c60abf98cf0bf7
alahmad-loay <loay.alahmad1@gmail.com>
2025-03-07 20:08:56 +0200

    404

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './NotFound.css'
-import { Link } from 'react-router-dom'
 const NotFound = () => {
   return (
     <div className="not-found d-flex flex-column justify-content-center align-items-center vh-100 text-center">
-      <h3 className=" fw-bold">404 - Not Found</h3>
+      <h3 className="text-danger fw-bold">404 - Not Found</h3>
       <Link to='/'><button className="btn btn-secondary mt-3">Back Home</button></Link>
     </div>
   )
