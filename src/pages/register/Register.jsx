6adf92f4060e1740551894539a82b37c80ded1f7
alahmad-loay2 <loay.alahmad3@gmail.com>
2025-04-16 00:39:38 +0300

    case fix

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "../login/Login-register.css";
+import "../login/login-register.css";
 import { auth, db } from "../../firebase/config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { setDoc, doc } from "firebase/firestore";
